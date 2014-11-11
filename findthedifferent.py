8c452c42b339098961d9876548d293c071735701
Gabriel A. Giménez <gabrielgimenez85@gmail.com>
2014-11-11 11:05:34 -0300

    progress

--- findthedifferent.py
+++ findthedifferent.py
@@ -13,6 +13,39 @@ import random
 
 class FindTheDifferent():
 	
+	def changeBackgroundColour(self, eventBox, colour):
+			eventBox.modify_bg(gtk.STATE_NORMAL, eventBox.get_colormap().alloc_color(colour))
+	
+	def checkCompletedExercise(self):
+		
+		checkFlag = True
+		for selectionState in self.selectionsState:
+			if selectionState['selectedIndex'] != selectionState['differentInex']:
+				checkFlag = False
+				break
+	
+		if checkFlag:
+			self.mainWindows.exerciseCompletedCallBack()
+		
+		
+	def letterSelectedCallBack(self,eventBox, *args):
+		
+		hVox = args[1]
+		vBoxExercises = args[2]
+		frame = args[3]
+		
+		indexHBox = vBoxExercises.child_get_property(frame, "position")
+		lastIndexSelected = self.selectionsState[indexHBox]['selectedIndex']
+		if lastIndexSelected != -1:
+			lastEventBoxSelected = hVox.get_children()[lastIndexSelected]
+			self.changeBackgroundColour(lastEventBoxSelected, "white")	
+		
+		currentIndexEventBox = hVox.child_get_property(eventBox, "position")
+		self.changeBackgroundColour(eventBox, "orange")
+		self.selectionsState[indexHBox]['selectedIndex'] = currentIndexEventBox
+		
+		self.checkCompletedExercise();
+	
 	def getWindow(self, exercise, mainWindows):
 		
 		self.mainWindows = mainWindows
@@ -28,207 +61,43 @@ class FindTheDifferent():
 		frameExercises.set_label_align(0.5, 0)
 		
 		
-		
 		vBoxWindows = gtk.VBox(False, 5)
-		vBoxExercises = gtk.VBox(True, 50)
+		vBoxExercises = gtk.VBox(True, 10)
 		
 		frameExercises.add(vBoxExercises)
 		
 		items = exercise.items
-		for index,  item in enumerate(items):
+		self.selectionsState = [None]*3
+		for index, item in enumerate(items):
 			
-			hVox = gtk.HBox(True, 50)
-			eventBox = gtk.EventBox()
-			letterLabel = gtk.Label(item.letter)
-			letterLabel.modify_font(pango.FontDescription("Courier Bold 80"))
-			eventBox.add(letterLabel)
-			hVox.pack_start(eventBox, False,True,50)
-			vBoxExercises.pack_start(hVox, False,True,50)
+			frame = gtk.Frame()
+			
+			hVox = gtk.HBox(True, 10)
+			frame.add(hVox)
+			
+			count = 0
+			until = 3
+			different = 2
+			self.selectionsState[index] = {"selectedIndex": -1,"differentInex":different}
+			while count <= until:
+				eventBox = gtk.EventBox()
+				letterLabel = gtk.Label(item.letter)
+				if count == different:
+					letterLabel = gtk.Label(item.differentLetter)
+				letterLabel.modify_font(pango.FontDescription("Courier Bold 60"))
+				eventBox.add(letterLabel)
+				self.changeBackgroundColour(eventBox, 'white')
+				eventBox.connect("button-press-event", self.letterSelectedCallBack, hVox, vBoxExercises, frame)
+				hVox.pack_start(eventBox, False,True,0)
+				count = count + 1
+				
+			vBoxExercises.pack_start(frame, False,True,30)
 		
-	
+		
 		vBoxWindows.pack_start(frameExercises, False,False,0)
 		windowFindTheDifferent.add_with_viewport(vBoxWindows)
 		
-		
 		return windowFindTheDifferent
-	
-	def selectFirtImage(self, firstEvenBox):
-		availableColour =  self.getAvailableSelectionColour()
-		self.changeBackgroundColour(firstEvenBox, availableColour['colour'])
-		self.setSelectionStateColour(self.imagesSelectionState, 0, availableColour)
-		self.currentImageSelected = 0
-		frameImageSelected = firstEvenBox.get_parent()
-		self.fakeSelection(frameImageSelected)
-		
-	def disorderPairs(self, pairList):
-		
-		imagesList = [None]*len(pairList)
-		pairsList = [None]*len(pairList)
-		
-		indexsList = range(len(pairList))
-		random.shuffle(indexsList)
-	
-		for index, pair in enumerate(pairList):
-			imagesList[index] = {"imagePath":pair.imagePath, "indexPair": indexsList[index]}
-			pairsList[indexsList[index]] = {"optionPair":pair.optionPair, "indexPair": index}
-		
-		return (imagesList, pairsList)
-	
-	def checkCompletedExercise(self):
-		result = True
-		for key,imageSelectionState in self.imagesSelectionState.items():
-			if (imageSelectionState['selected'] != imageSelectionState['pair']) or (self.pairSelectionState[key]['selected'] != self.pairSelectionState[key]['pair']) :
-				result = False
-				break
-		if result:
-			self.mainWindows.exerciseCompletedCallBack()
-	
-
-	def getAvailableSelectionColour(self):
-		for colour in self.colours:
-			if colour['available']:
-				return colour
-	
-	def setAvailableColour(self, colour):
-		self.colours[self.colours.index(colour)]['available'] = True
-	
-	def setUnavailableColour(self, colour):
-		self.colours[self.colours.index(colour)]['available'] = False
 		
 	
-	def imageSelectedCallBack(self, imageEventBox, *args):
 		
-		frameImageSelected = imageEventBox.get_parent()
-		vBoxImages = imageEventBox.get_parent().get_parent()
-		
-		allImagesFrames = vBoxImages.get_children()
-		
-		indexImageSelected = vBoxImages.child_get_property(frameImageSelected, "position")
-		self.lastImageSelected = self.currentImageSelected
-		self.currentImageSelected = indexImageSelected
-		
-		vBoxPairs = args[1]
-		'''Se des-selecciona el par selecciondo previamente'''
-		if self.currentPairSelected != -1:
-			framePairSelected = vBoxPairs.get_children()[self.currentPairSelected]
-			self.fakeUnselection(framePairSelected)
-		
-		# Revisamos si la ultima imagen seleccionada no fue asociada
-		if self.lastImageSelected != -1 and self.imagesSelectionState[self.lastImageSelected]['selected'] == -1:
-			# No se ha asociado nada, volvemos a a poner a blanco el bg colour
-			lastImageEvenBoxSelected = allImagesFrames[self.lastImageSelected].get_children()[0]
-			self.changeBackgroundColour(lastImageEvenBoxSelected, 'white')
-			self.setSelectionStateColour(self.imagesSelectionState, indexImageSelected, None)
-			
-			
-		# Revisamos si ya existe una asociacion'''
-		if self.imagesSelectionState[indexImageSelected]['selected'] == -1:
-			# Aun no existe una asociación
-			colorAvailable = self.getAvailableSelectionColour()
-			self.changeBackgroundColour(imageEventBox, colorAvailable['colour'])
-			self.setSelectionStateColour(self.imagesSelectionState, indexImageSelected, colorAvailable)
-			
-		#cambiamos los colores de los bordes (frames) para notificar la seleccion
-		self.fakeSelection(frameImageSelected)
-		lastFrameImageSelected = allImagesFrames[self.lastImageSelected]
-		self.fakeUnselection(lastFrameImageSelected)
-		self.fakeUnselection
-		
-		#Comprabamos la finalización del ejercicio
-		self.checkCompletedExercise()
-			
-	def pairSelectedCallBack(self, pairEventBox, *args):
-
-		vBoxImages = args[1]
-		allFramesImages = vBoxImages.get_children()
-				
-		framePairSelected = pairEventBox.get_parent()
-		vBoxPairs = framePairSelected.get_parent()
-		
-		allPairFrames = vBoxPairs.get_children()
-		
-		indexPairSelected = vBoxPairs.child_get_property(framePairSelected, "position")
-		self.lastPairSelected = self.currentPairSelected
-		self.currentPairSelected = indexPairSelected
-		
-		lastPairSelectionState = None
-		if self.lastPairSelected != -1:
-			lastPairSelectionState = self.pairSelectionState[self.lastPairSelected]
-		
-		pairIndexCurrentImageSelected = -1
-		imageEventBoxCurremtSelected = None
-		if self.currentImageSelected != -1:
-			pairIndexCurrentImageSelected = self.imagesSelectionState[self.currentImageSelected]['selected']
-			imageEventBoxCurremtSelected = self.imagesSelectionState[self.currentImageSelected]['colour']
-		
-		pairEventBoxCurrentImageSelected = None
-		if self.currentImageSelected != -1 and pairIndexCurrentImageSelected != -1:
-			pairEventBoxCurrentImageSelected = allPairFrames[self.imagesSelectionState[self.currentImageSelected]['selected']].get_children()[0]
-		
-		
-		# Verificamos que el ultimo par seleccionado no tenga una asocicion
-		if self.lastPairSelected != -1 and lastPairSelectionState['selected'] == -1:
-			lastPairEventBoxSelected = allPairFrames[self.lastPairSelected].get_children()[0]
-			self.changeBackgroundColour(self, lastPairEventBoxSelected, 'white')
-			self.setAvailableColour(lastPairSelectionState['colour'])
-			self.setSelectionStateColour(self.pairSelectionState, self.lastPairSelected, None)
-			
-		
-		#Comprobamos si hay alguna imagen seleccionada 
-		if self.currentImageSelected != -1:
-			#usamos el color de la imagen seleccionada como bg
-			colourImageSelected = self.imagesSelectionState[self.currentImageSelected]['colour']
-			self.changeBackgroundColour(pairEventBox, colourImageSelected['colour'])
-			self.setSelectionStateColour(self.pairSelectionState, indexPairSelected, colourImageSelected )
-			
-					
-			#La imagen asociada al par poseía otro par asociado anteriormente
-			if pairIndexCurrentImageSelected != -1 and pairIndexCurrentImageSelected != self.currentPairSelected:
-				#des-asociamos el par aterior
-				currentPairEventBoxSelected = allPairFrames[pairIndexCurrentImageSelected].get_children()[0]
-				self.changeBackgroundColour(currentPairEventBoxSelected, 'white')
-				self.setAvailableColour(self.pairSelectionState[pairIndexCurrentImageSelected]['colour'])
-				self.setSelectionStateColour(self.pairSelectionState, pairIndexCurrentImageSelected, None )
-				self.pairSelectionState[pairIndexCurrentImageSelected]['selected'] = -1
-				
-				
-			#El par seleccionado ya fue asociado por otra imagen, la cual no es la actualmente seleccionada
-			if self.pairSelectionState[indexPairSelected]['selected'] != -1 and self.pairSelectionState[indexPairSelected]['selected'] != self.currentImageSelected:
-				#des-asociamos la imagen anterior asociada al par
-				imagePairSelectedEventBox= allFramesImages[self.pairSelectionState[indexPairSelected]['selected']].get_children()[0]
-				self.changeBackgroundColour(imagePairSelectedEventBox,'white')
-				self.setAvailableColour(self.imagesSelectionState[self.pairSelectionState[indexPairSelected]['selected']]['colour'])
-				self.setSelectionStateColour(self.imagesSelectionState, self.pairSelectionState[indexPairSelected]['selected'], None )
-				self.imagesSelectionState[self.pairSelectionState[indexPairSelected]['selected']]['selected'] = -1
-			
-			#guardamos los datos de la asociacion actuales de ambos lados
-			self.pairSelectionState[indexPairSelected]['selected'] = self.currentImageSelected
-			self.imagesSelectionState[self.currentImageSelected]['selected'] = indexPairSelected
-			self.setUnavailableColour(colourImageSelected)	
-		else:
-			# TODO: No hay imagen seleccionada, re-ver!
-			'''colourAvailable =  self.getAvailableSelectionColour()
-			pairEventBox.modify_bg(gtk.STATE_NORMAL, pairEventBox.get_colormap().alloc_color(colourAvailable['colour']))
-			self.pairSelectionState[indexPairSelected]['colour'] = colourAvailable'''
-			
-		#cambiamos los colores de los bordes (frames) para notificar la seleccion
-		self.fakeSelection(framePairSelected)
-		lastFramePairSelected = allPairFrames[self.lastPairSelected]
-		self.fakeUnselection(lastFramePairSelected)
-		
-		#Comprabamos la finalización del ejercicio
-		self.checkCompletedExercise()
-	
-	def fakeSelection(self, frame):
-		frame.modify_bg(gtk.STATE_NORMAL, frame.get_colormap().alloc_color('black'))
-	
-	def fakeUnselection(self, frame)	:
-		frame.modify_bg(gtk.STATE_NORMAL, frame.get_colormap().alloc_color('white'))
-	
-	def changeBackgroundColour(self, eventBox, colour):
-		eventBox.modify_bg(gtk.STATE_NORMAL, eventBox.get_colormap().alloc_color(colour))
-		
-		
-	def setSelectionStateColour(self,selectionState, index, colour):
-		selectionState[index]['colour'] = colour
-	
